4379c5bf16778948421599b9e59c013dd85826ad
mohamine78 <mohamine78200@outlook.fr>
2024-12-17 14:00:11 +0100

    Exercice user corrige

--- src/composants/containers/User/User.jsx
+++ src/composants/containers/User/User.jsx
@@ -1,17 +1,11 @@
+import FullName from "../Fullname/Fullname";
+import Hobbies from "../Hobbies/Hobbies";
+
 export default function User() {
-    const users = [
-        { firstname: "John", lastname: "Jones", hobbies: ["Voyage","Photographie","Sport"]}, 
-        { firstname: "Jack", lastname: "Will", hobbies: ["Cuisine","Guitare","Lecture"]}, 
-    ];
     return (
         <div>
-            <p>Nom user 1: {users[0].firstname}
-            prénom user 1: {users[0].lastname}
-            Hobbie user 1: {users[0].hobbies}</p>
-
-        <p>Nom user 2: {users[1].firstname}
-        prénom user 2: {users[1].lastname}
-        hobbie user 2: {users[1].hobbies}</p>
-    </div>
-    );
+            <FullName />
+            <Hobbies />
+        </div>
+    )
 }
---
+++ src/composants/ui/Lastname/Lastname.jsx
@@ -0,0 +1,3 @@
+export default function LastName() {
+    return (<p>Mohammed</p>)
+}
