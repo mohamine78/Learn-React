e69f36ac7022c616749daab62915e4af344baf36
mohamine78 <mohamine78200@outlook.fr>
2024-12-17 11:22:32 +0100

    export default fixed

---
+++ src/composants/containers/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+            <footer>
+    <a href="#">Mentions Légales</a>
+    <a href="#" >Politique de Confidentialité</a>
+    <a href="#">Contact</a>
+</footer>
+
+    )
+}
---
+++ src/composants/containers/Header/Header.jsx
@@ -0,0 +1,6 @@
+export default function Header(){
+    return (<header>
+        <h1>Bonjour</h1>
+        <p>Bonjour tout le monde</p>
+    </header>)
+}
